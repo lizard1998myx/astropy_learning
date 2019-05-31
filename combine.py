25cdd3c2a06c5f09cfcb80eb02961cf89e612d7e
Sgt.Lizard <lizard1998myx@gmail.com>
2019-05-31 14:52:01 +0800

    combine V2.3
    + improve format
    + fix some bugs

--- combine.py
+++ combine.py
@@ -1,17 +1,17 @@
-#Combine two images with inverse-variance weighting V2.2
-#Yuxi Meng 2019-05-30
+#Combine two images with inverse-variance weighting V2.3
+#Yuxi Meng 2019-05-31
 #
+# armory version
 
 from astropy.io import fits
 
-#test the combine function
-def test_combine():
-    imagelist = ["ppr2_briggs_-0.5-t0500-XX-image.fits",
-                 "ppr2_briggs_-0.5-t0500-YY-image.fits"]
-    beamlist = ["ppr2_briggs_0.5-XX-dirty_beamXX.fits",
-                "ppr2_briggs_0.5-XX-dirty_beamYY.fits"]
-    output = "ppr2_briggs_-0.5-t0500-image_astropy_combination_V2.2.fits"
-    combine(imagelist, beamlist, output)
+#an example of combining images
+def example_combine():
+	imagelist = ["image_XX.fits", "image_YY.fits"]
+	beamlist = ["beam_XX.fits", "beam_YY.fits"]
+	output = "combination_V2.0.fits"
+	combine(imagelist, beamlist, output)
+	return
 
 #calculate the inverse-variance weighting
 #aggregate radom variables to minimize the variance of the weighted average
@@ -30,11 +30,8 @@ def inv_var_weighting ( ylist, sigmalist ):
 
 	return numerator/denominator
 
-
 #combine two images
 def combine ( imagelist, beamlist, output ):
-
-#	from astropy.io import fits
 
 	if len(imagelist) != len(beamlist):
 		print("Unequal list length.")
@@ -44,21 +41,19 @@ def combine ( imagelist, beamlist, output ):
 	hdul = fits.open(str(imagelist[0]))
 	data = hdul[0].data
 
-	ylist = []		#store a list of variables
-	sigmalist = []	#store a list of variance
+	ylist = []
+	sigmalist = []
 
 	for i in range(len(imagelist)):
 
-		# read the data tables
+		# read the data
 		with fits.open(str(imagelist[i])) as imagehdul:
 			imagedata = imagehdul[0].data
 		with fits.open(str(beamlist[i])) as beamhdul:
 			beamdata = beamhdul[0].data
 		
 		# make ylist and sigmalist
-		# y = Image instead of Image/Beam
-		# sigma = 1/Beam
-		ylist.append(imagedata/beamdata)
+		ylist.append(imagedata)
 		sigmalist.append(1/beamdata)
 
 	#calculate
@@ -70,6 +65,7 @@ def combine ( imagelist, beamlist, output ):
 
 	return
 
-
-
-test_combine()
+#this is nothing but a Easter egg! :)
+def hello():
+	print("hello")
+	return
