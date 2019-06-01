480f7f8b2a3b1939c46b15bb024f734941086fa9
Sgt.Lizard <lizard1998myx@gmail.com>
2019-06-01 21:40:02 +0800

    plotdata V1.1
    + separate the data-loader and basic plotter function, easier to load multiple plots
    + use one single color for each set of data, use dashed line to distinguish subsets
    + markers added, easier to find the data points
    + change the size of the figure
    - do not use multiple colors, the plot looks less interesting

--- plotdata.py
+++ plotdata.py
@@ -1,14 +1,33 @@
-# plot the data made by summary.py
-# 2019-05-31 Yuxi Meng
+# plot the data made by summary.py V1.1
+# 2019-06-01 Yuxi Meng
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# read two files
+# read files
 file_default = pd.read_csv('output_default.csv')
-file_minuv40 = pd.read_csv('output_minUV40.csv')
+file_minuv20 = pd.read_csv('output_minuv20.csv')
+file_minuv40 = pd.read_csv('output_minuv40.csv')
+file_minuv80 = pd.read_csv('output_minuv80.csv')
 
+def plotter(ax, xlist, ylist, color, label, **param_dict):
+    """
+    plotter()
+    
+    basic plotter
+    automatically plot two lines
+    
+    they are of same color but different styles
+    """
+    ax.plot(xlist[:9], ylist[:9], **param_dict,
+            color=color, marker='.', linestyle='dashed',
+            label=str(label)+"_all")
+    ax.plot(xlist[-9:], ylist[-9:],
+            color=color, marker='.', **param_dict,
+            label=str(label)+"_center")
+    
+    return
 
 def datainput(ax, xstring, ystring, **param_dict):
     """
@@ -17,6 +36,8 @@ def datainput(ax, xstring, ystring, **param_dict):
     put the data into an axis
     this axis is used for future plotting
     parameter can be set as well (optional)
+    the plot will be gridded
+    use ax.grid() again to cancel it
     
     data are from the two given files
     color and label are given
@@ -24,26 +45,24 @@ def datainput(ax, xstring, ystring, **param_dict):
     
     xstring = str(xstring)
     ystring = str(ystring)
-    #ylabel = str(ylabel)
     
-    ax.plot(file_default[xstring][:9],
-             file_default[ystring][:9],
-             'xkcd:pink',
-             **param_dict, label='default_all')
-    ax.plot(file_default[xstring][-9:],
-             file_default[ystring][-9:],
-             'xkcd:light red',
-             **param_dict, label='default_center')
-    ax.plot(file_minuv40[xstring][:9],
-             file_minuv40[ystring][:9],
-             'xkcd:sky blue',
-             **param_dict, label='minuv40_all')
-    ax.plot(file_minuv40[xstring][-9:],
-             file_minuv40[ystring][-9:],
-             'xkcd:blue',
-             **param_dict, label='minuv40_center')
-    #ax.set_ylabel(ylabel)
-    #ax.legend(loc='best')
+    # default red
+    plotter(ax, file_default[xstring], file_default[ystring],
+            "xkcd:red", "default", **param_dict)
+ 
+    # minuv 20 golden
+    plotter(ax, file_minuv20[xstring], file_minuv20[ystring],
+            "xkcd:golden", "minuv20", **param_dict)
+        
+    # minuv 40 green
+    plotter(ax, file_minuv40[xstring], file_minuv40[ystring],
+            "xkcd:green", "minuv40", **param_dict)
+    
+    # minuv 80 blue
+    plotter(ax, file_minuv80[xstring], file_minuv80[ystring],
+            "xkcd:blue", "minuv80", **param_dict)
+    
+    ax.grid()
     return
 
 
@@ -60,8 +79,7 @@ The plotting is done with following steps:
 """
 
 # First Figure: major plot of noise feature
-#
-fig0, ax0 = plt.subplots()
+fig0, ax0 = plt.subplots(figsize=(10, 8))
 datainput(ax0, 'local_rms_mean', 'psf_b')
 
 # set the note and legend
@@ -69,7 +87,6 @@ ax0.set_title('Noise Feature')
 ax0.set_ylabel('Synthesis Beam (arcsec)')
 ax0.set_xlabel('Local RMS')
 ax0.legend(loc='lower right')
-ax0.grid()
 
 # special: set the annotation of briggs value
 
@@ -90,30 +107,35 @@ fig0.tight_layout()
 
 
 # Second Figure: 2x2 subplots
-#
-fig1, ((ax1, ax2),(ax3,ax4)) = plt.subplots(2,2)
+fig1, ((ax1, ax2),(ax3,ax4)) = plt.subplots(2,2, figsize=(10, 8))
 
-# 1st plot: source count
-datainput(ax1, 'briggs', 'source_count')
-ax1.set_ylabel('Source Count')
-#ax1.grid()
-ax1.set_yticks(np.arange(500, 3001, 500))
+# 1st plot: local_rms
+datainput(ax1, 'briggs', 'local_rms_mean')
+ax1.set_ylabel('Local RMS')
+ax1.set_xlabel('Briggs Parameter')
+ax1.grid()
+ax1.legend()
 
-# 2nd plot: SNR
-datainput(ax2, 'briggs', 'SNR_mean')
-ax2.set_ylabel('SN ratio')
-ax2.set_yticks(np.arange(12, 17.1, 1))
+# 2nd plot: peak_flux
+datainput(ax2, 'briggs', 'peak_flux_mean')
+ax2.set_ylabel('Peak Flux')
+ax2.set_xlabel('Briggs Parameter')
+ax2.grid()
 
+# 3rd plot: source count
+datainput(ax3, 'briggs', 'source_count')
+ax3.set_ylabel('Source Count')
+ax3.set_xlabel('Briggs Parameter')
+ax3.grid()
+ax3.set_yticks(np.arange(500, 3001, 500))
 
-# 3rd plot: local_rms
-datainput(ax3, 'briggs', 'local_rms_mean')
-ax3.set_ylabel('Local RMS')
-ax3.set_xlabel('Briggs')
+# 4th plot: SNR
+datainput(ax4, 'briggs', 'SNR_mean')
+ax4.set_ylabel('SN ratio')
+ax4.set_xlabel('Briggs Parameter')
+ax4.grid()
+ax4.set_yticks(np.arange(12, 17.1, 1))
 
-# 4th plot: peak_flux
-datainput(ax4, 'briggs', 'peak_flux_mean')
-ax4.set_ylabel('Peak Flux')
-ax4.set_xlabel('Briggs')
-
+# arrange and show the figure
 fig1.tight_layout()
 plt.show()
